5936c25996729bb8ef002bf67c238774388bb8b3
suhanyujie <suhanyujie@qq.com>
2023-05-06 21:23:51 +0800

    update doc

--- src/panel.py
+++ src/panel.py
@@ -30,27 +30,38 @@ class Panel(QWidget):
 
         self.btn = QPushButton("选择 xlsx 文件目录", self)
         self.btn.move(20, 20)
-        res = self.btn.clicked.connect(self.showDialogInput)
-        print(res)
+        self.btn.clicked.connect(self.showDialogInput)
+        self.nameLineEdit = QLineEdit(self)
+        self.nameLineEdit.move(160, 20)
+        self.nameLineEdit.setMinimumWidth(280)
 
         self.btn1 = QPushButton("选择导出目录", self)
         self.btn1.move(20, 50)
         self.btn1.clicked.connect(self.showDialogOutput)
+        self.nameLineEdit1 = QLineEdit(self)
+        self.nameLineEdit1.move(100, 50)
+        self.nameLineEdit1.setMinimumWidth(200)
 
         self.btn2 = QPushButton("开始转换", self)
-        self.btn2.setMinimumWidth(100)
+        self.btn2.setMinimumWidth(120)
         self.btn2.setMinimumHeight(80)
-        self.btn2.move(300, 120)
+        self.btn2.move(300, 200)
         self.btn2.clicked.connect(self.startConvert)
 
     def showDialogInput(self) -> str:
         fname = QFileDialog.getExistingDirectory(directory="./")
+        if fname == "":
+            fname = "./"
         self.inputDir = fname
+        self.nameLineEdit.setText(self.inputDir)
         return fname
 
     def showDialogOutput(self) -> str:
         fname = QFileDialog.getExistingDirectory(directory="./output")
+        if fname == "":
+            fname = "./output"
         self.outputDir = fname
+        self.nameLineEdit1.setText(self.outputDir)
         return fname
 
     def startConvert(self):
