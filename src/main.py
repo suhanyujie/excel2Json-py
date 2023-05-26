b7d945ffdf85122a3dc945b3a6a74e927db48598
suhanyujie <suhanyujie@qq.com>
2023-05-26 13:49:24 +0800

    update some comment line

--- src/main.py
+++ src/main.py
@@ -3,13 +3,11 @@ from PyQt6.QtWidgets import (
     QApplication,
 )
 from panel import Panel
-from helper import ConfHelper, FileHelper
+
+# from helper import ConfHelper, FileHelper
 
 
 def main():
-    # for test
-    # ConfHelper.get_history()
-
     app = QApplication(sys.argv)
     panel = Panel()
     panel.show()
